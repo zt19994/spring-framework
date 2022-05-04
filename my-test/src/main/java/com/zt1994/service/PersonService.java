197f6b1e35159d81b67b792d7d16d5f000081693
zt19994 <zt191610942@.com>
2022-05-04 23:33:12 +0800

    注解方式注入属性

--- my-test/src/main/java/com/zt1994/service/PersonService.java
+++ my-test/src/main/java/com/zt1994/service/PersonService.java
@@ -3,6 +3,7 @@ package com.zt1994.service;
 import com.zt1994.dao.PersonDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,8 +23,12 @@ public class PersonService {
 	@Qualifier(value = "personDao1Impl")
 	private PersonDao personDao;
 
+	@Value(value = "abc")
+	private String name;
+
 	public void add() {
 		System.out.println("Service add");
+		System.out.println(name);
 		personDao.add();
 	}
 }
